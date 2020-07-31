840c28c86f091bc2c0b62b225b324faab327582c
Ygor <ygor.maniz@gmail.com>
2020-07-31 14:41:13 -0300

    refactor: group imports in index files

--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -1,5 +1,5 @@
 import { AuthenticationParams } from '@/domain/usecases/authentication'
-import { AccountModel } from '@/domain/models/account-model'
+import { AccountModel } from '@/domain/models'
 
 import faker from 'faker'
 
